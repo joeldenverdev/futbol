1a394878c0896edfaa52db57abe70843e9e2e8f3
Joel Grant <joelmgrant@me.com>
2021-11-03 15:34:57 -0600

    Test: Add #best_season test

--- spec/stat_tracker_spec.rb
+++ spec/stat_tracker_spec.rb
@@ -1,9 +1,10 @@
-require_relative './lib/stat_tracker'
+require './lib/stat_tracker'
 require 'csv'
-require_relative './lib/game_team'
-require_relative './lib/game'
-require_relative './lib/team'
-require_relative './runner'
+require './lib/game_team'
+require './lib/game'
+require './lib/team'
+require './runner'
+require 'rspec'
 
 
 # Game Statistics Tests
@@ -18,43 +19,60 @@ require_relative './runner'
 # Team Statistics Tests
 describe StatTracker do
   before(:each) do
-    game_path = './data/games.csv'
-    team_path = './data/teams.csv'
-    game_teams_path = './data/game_teams.csv'
+    game_path = './data/games_test.csv'
+    team_path = './data/teams_test.csv'
+    game_teams_path = './data/game_teams_test.csv'
+    @games = {}
+    @teams = {}
+    @game_teams = {}
     locations = {
       games: game_path,
       teams: team_path,
       game_teams: game_teams_path
     }
+
     stat_tracker = StatTracker.from_csv(locations)
-    game_1 = Game.new()
+    stat_tracker[0].read_game_stats(game_path)
+    stat_tracker[0].read_team_stats(team_path)
+    stat_tracker[0].read_game_teams_stats(game_teams_path)
+    require 'pry'; binding.pry
+
   end
 
   describe '#team_info' do
     it 'returns team_id, franchise_id, team_name, abbr., and link' do
+      expected = {
+        team_id => 27,
+        franchise_id => 28,
+        team_name => "San Jose Earthquakes",
+        abbreviation => "SJ",
+        link => "/api/v1/teams/27"
+      }
+      require 'pry'; binding.pry
+      expect(stat_tracker[0].team_info).to eq(expected)
     end
   end
 
   describe '#best_season' do
-    it 'returns the season with the highest win percentage for a team' do
+    xit 'returns the season with the highest win percentage for a team' do
 
     end
   end
 
   describe '#worst_season' do
-    it 'returns the season with the lowest win percentage for a team' do
+    xit 'returns the season with the lowest win percentage for a team' do
 
     end
   end
 
   describe '#average_win_percentage' do
-    it 'returns the average win percentage of all games for a team' do
+    xit 'returns the average win percentage of all games for a team' do
 
     end
   end
 
   describe '#most_goals_scored' do
-    it 'returns the highest nuber of goals a particular team has scored in a single game' do
+    xit 'returns the highest nuber of goals a particular team has scored in a single game' do
 
     end
   end
@@ -66,13 +84,13 @@ describe StatTracker do
   end
 
   describe '#favorite_opponent' do
-    it 'returns the name of the opponent that has the lowest win percentage against the given team' do
+    xit 'returns the name of the opponent that has the lowest win percentage against the given team' do
 
     end
   end
 
   describe '#rival' do
-    it 'returns the name of the opponent that has the highest win percentage against the given team' do
+    xit 'returns the name of the opponent that has the highest win percentage against the given team' do
 
     end
   end
