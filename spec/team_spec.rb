47c2339677d2f57608f466ebb42d09d020e3d4e1
Brad Breiten <jonathan@breiten.us>
2021-11-08 21:58:13 -0700

    Add initialize test for team class

--- spec/team_spec.rb
+++ spec/team_spec.rb
@@ -3,29 +3,53 @@ require './lib/team'
 
 describe Team do
 
-  let(:team) {Team.new({
+  let(:team_obj) {Team.new({
     team_id: "1",
-    franchise_id: "23",
-    team_name: "Atlanta United",
+    franchiseid: "23",
+    teamname: "Atlanta United",
     abbreviation: "ATL",
     stadium: "Mercedes-Benz",
     link: "/api/v1/teams/1"
     })}
 
   before(:each) do
-    team_path = './data/teams_tester.csv'
+    team_path = './data/teams.csv'
 
     locations = {
       teams: team_path
     }
 
     CSV.foreach(locations[:teams], headers: true, header_converters: :symbol) do |row|
-      team = Team.new(row)
+      @team = Team.new(row)
     end
   end
 
   it 'exists' do
     expect(@team).to be_instance_of(Team)
+  end
+
+  it 'has team_id' do
+    expect(team_obj.team_id).to eq "1"
+  end
+
+  it 'has franchise_id' do
+    expect(team_obj.franchise_id).to eq "23"
+  end
+
+  it 'has team_name' do
+    expect(team_obj.team_name).to eq "Atlanta United"
+  end
+
+  it 'has abbreviation' do
+    expect(team_obj.abbreviation).to eq "ATL"
+  end
+
+  it 'has stadium' do
+    expect(team_obj.stadium).to eq "Mercedes-Benz"
+  end
+
+  it 'has link' do
+    expect(team_obj.link).to eq "/api/v1/teams/1"
   end
 
 end
