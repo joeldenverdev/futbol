ec1ac5204333087cb7fdc0d0b1827a5fb0557592
Brad Breiten <jonathan@breiten.us>
2021-11-08 19:45:37 -0700

    Update stat tracker methods

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -76,306 +76,61 @@ class StatTracker
   end
 
   # League Statistics
-
   def count_of_teams
     @league_stats.count_of_teams
   end
 
   def best_offense
     @league_stats.best_offense
-    id = @game_teams.max_by do |team|
-      average_goals_per_game_by_team(team.team_id)
-    end.team_id
-    team_name_by_id(id)
   end
 
   def worst_offense
-    id = @game_teams.min_by do |team|
-      average_goals_per_game_by_team(team.team_id)
-    end.team_id
-    team_name_by_id(id)
+    @league_stats.worst_offense
   end
 
-  def games_away(team_id)
-    games = games_by_team(team_id)
-    away = []
-    games.each do |game|
-      if game.hoa == "away"
-        away << game
-      end
-    end
-    away
+  def highest_scoring_visitor
+    @league_stats.highest_scoring_visitor
+  end
+
+  def highest_scoring_home_team
+    @league_stats.highest_scoring_home_team
+  end
+
+  def lowest_scoring_visitor
+    @league_stats.lowest_scoring_visitor
+  end
+
+  def lowest_scoring_home_team
+    @league_stats.lowest_scoring_home_team
   end
 
 # Season Statistics
-
-   def season_games(season)
-    @games.find_all do |game|
-      game.season == season
-    end
-  end
-
   def winningest_coach(season)
     @season_stats.winningest_coach(season)
-    filtered_game_teams = []
-    @game_teams.each do |game_team|
-      season_games(season).each do |game|
-        if game.game_id == game_team.game_id
-          filtered_game_teams << game_team
-        end
-      end
-    end
-
-    coach_win_count = Hash.new(0)
-    filtered_game_teams.each do |game_team|
-      if game_team.result == "WIN"
-        coach_win_count[game_team.head_coach] += 1
-      end
-    end
-
-    coach_total_game_count = Hash.new(0)
-    filtered_game_teams.each do |game_team|
-      coach_total_game_count[game_team.head_coach] += 1
-    end
-
-    winning_percent = Hash.new(0)
-    coach_total_game_count.each_key do |key|
-      winning_percent[key] = coach_win_count[key] / coach_total_game_count[key].to_f * 100
-    end
-
-    winning_coach = winning_percent.max_by { |key, value| value }[0]
   end
 
   def worst_coach(season)
-    filtered_game_teams = []
-    @game_teams.each do |game_team|
-      season_games(season).each do |game|
-        if game.game_id == game_team.game_id
-          filtered_game_teams << game_team
-        end
-      end
-    end
-
-    coach_win_count = Hash.new(0)
-    filtered_game_teams.each do |game_team|
-      if game_team.result == "WIN"
-        coach_win_count[game_team.head_coach] += 1
-      end
-    end
-
-    coach_total_game_count = Hash.new(0)
-    filtered_game_teams.each do |game_team|
-      coach_total_game_count[game_team.head_coach] += 1
-    end
-
-    losing_percent = Hash.new(0)
-    coach_total_game_count.each_key do |key|
-      losing_percent[key] = coach_win_count[key] / coach_total_game_count[key].to_f * 100
-    end
-    losing_coach = losing_percent.min_by { |key, value| value }[0]
+    @season_stats.worst_coach(season)
   end
 
 
   def most_accurate_team(season)
-    filtered_teams = []
-    @teams.each do |team|
-      @game_teams.each do |game_team|
-        season_games(season).each do |game|
-          if game_team.team_id == team.team_id && game.game_id == game_team.game_id
-            filtered_teams << team
-          end
-        end
-      end
-    end
-
-    goal_count = Hash.new(0)
-    filtered_teams.each do |game_team|
-      goal_count[game_team.team_id] += 1
-    end
-
-    shots_count = Hash.new(0)
-    filtered_teams.each do |game_team|
-      shots_count[game_team.team_id] += 1
-    end
-
-    best_ratio = Hash.new(0)
-    filtered_teams.each do |game_team|
-      goal_count.each_value do |goal|
-        shots_count.each_value do |shot|
-          if goal >= shot
-            best_ratio[game_team.team_id] += 1
-          end
-        end
-      end
-    end
-
-    best_team_id = best_ratio.max_by { |key, value| value }[0]
-
-    team_name = @teams.find do |team|
-      team.team_id == best_team_id
-    end
-    team_name.team_name
+    @season_stats.most_accurate_team(season)
   end
 
   def least_accurate_team(season)
-    filtered_teams = []
-    @teams.each do |team|
-      @game_teams.each do |game_team|
-        season_games(season).each do |game|
-          if game_team.team_id == team.team_id && game.game_id == game_team.game_id
-            filtered_teams << team
-          end
-        end
-      end
-    end
-
-    goal_count = Hash.new(0)
-    filtered_teams.each do |game_team|
-      goal_count[game_team.team_id] += 1
-    end
-
-    shots_count = Hash.new(0)
-    filtered_teams.each do |game_team|
-      shots_count[game_team.team_id] += 1
-    end
-
-    worst_ratio = Hash.new(0)
-    filtered_teams.each do |game_team|
-      goal_count.each_value do |goal|
-        shots_count.each_value do |shot|
-          if goal <= shot
-            worst_ratio[game_team.team_id] += 1
-          end
-        end
-      end
-    end
-
-    worst_team_id = worst_ratio.min_by { |key, value| value }[0]
-
-    team_name = @teams.find do |team|
-      team.team_id == worst_team_id
-    end
-    team_name.team_name
+    @season_stats.least_accurate_team(season)
   end
 
   def most_tackles(season)
-    filtered_game_teams = []
-    @game_teams.each do |game_team|
-      season_games(season).each do |game|
-        if game.game_id == game_team.game_id
-          filtered_game_teams << game_team
-        end
-      end
-    end
-
-    total_team_tackles = Hash.new(0)
-    filtered_game_teams.each do |game_team|
-      total_team_tackles[game_team.team_id] += game_team.tackles
-    end
-
-    team_id_tackles = total_team_tackles.max_by { |key, value| value }[0]
-
-    team_name = @teams.find do |team|
-      team.team_id == team_id_tackles
-    end
-
-    team_name.team_name
+    @season_stats.most_tackles(season)
   end
 
   def fewest_tackles(season)
-    filtered_game_teams = []
-    @game_teams.each do |game_team|
-      season_games(season).each do |game|
-        if game.game_id == game_team.game_id
-          filtered_game_teams << game_team
-        end
-      end
-    end
-
-    total_team_tackles = Hash.new(0)
-    filtered_game_teams.each do |game_team|
-      total_team_tackles[game_team.team_id] += game_team.tackles
-    end
-
-    team_id_tackles = total_team_tackles.min_by { |key, value| value }[0]
-
-    team_name = @teams.find do |team|
-      team.team_id == team_id_tackles
-    end
-    team_name.team_name
-  end
-
-  def average_away_score(team_id)
-    games = games_away(team_id)
-    away_scores = games_away(team_id).map do |game|
-      game.goals
-    end
-    avg = away_scores.sum.to_f / games.length.to_f
-    avg.round(1)
-  end
-
-  def highest_scoring_visitor
-    id = @game_teams.max_by do |game|
-      average_away_score(game.team_id)
-    end.team_id
-    @teams.find do |team|
-      id == team.team_id
-    end.team_name
-  end
-
-  def games_home(team_id)
-    games = games_by_team(team_id)
-    home = []
-    games.each do |game|
-      if game.hoa == "home"
-        home << game
-      end
-    end
-    home
-  end
-
-  def average_home_score(team_id)
-    games = games_home(team_id)
-    home_scores = games_home(team_id).map do |game|
-      game.goals
-    end
-    avg = home_scores.sum.to_f / games.length.to_f
-    avg.round(1)
-  end
-
-  def highest_scoring_home_team
-    id = @game_teams.max_by do |game|
-      average_home_score(game.team_id)
-    end.team_id
-
-    @teams.find do |team|
-      id == team.team_id
-    end.team_name
-  end
-
-  def lowest_scoring_visitor
-    id = @game_teams.min_by do |game|
-      average_away_score(game.team_id)
-    end.team_id
-
-    @teams.find do |team|
-      id == team.team_id
-    end.team_name
-  end
-
-  def lowest_scoring_home_team
-    id = @game_teams.min_by do |game|
-      average_home_score(game.team_id)
-    end.team_id
-
-    @teams.find do |team|
-      id == team.team_id
-    end.team_name
-
+    @season_stats.fewest_tackles(season)
   end
 
   # Team Statistics
-
   def team_info(team_id)
     @team_stats.team_info(team_id)
   end
